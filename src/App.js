1d9da23c9dbb371a4b9b68bdc10e18ad3e3efbd4
Michell Ayala <starships@outlook.com>
2020-09-12 15:47:29 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState({
+    1: { id: 1, name: "John Doe" },
+    2: { id: 2, name: "jenny doe" },
+  });
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isConfirmation, setIsConfirmation] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  const handleClickDeleteUser = (id) => () => {
+    setUserId(id);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleClickDelete = () => {
+    setIsConfirmation(true);
+  };
+
+  const handleClickCancel = () => {
+    setIsDeleteModalOpen(false);
+    setIsConfirmation(false);
+  };
+
+  const handleClickConfirmDelete = () => {
+    const updatedUsers = { ...users };
+    delete updatedUsers[userId];
+
+    setUsers(updatedUsers);
+    setIsConfirmation(false);
+    setIsDeleteModalOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Object.keys(users).map((userId) => (
+        <div key={userId}>
+          Name: {users[userId].name}
+          <button onClick={handleClickDeleteUser(userId)}>Delete</button>
+        </div>
+      ))}
+
+      {isDeleteModalOpen && !isConfirmation && (
+        <section className="modal">
+          <div className="modal__container">
+            <header>
+              <h2>Delete User</h2>
+            </header>
+            <div className="modal__content">
+              You are about to remove a user, are you sure about that?
+            </div>
+            <footer>
+              <button onClick={handleClickCancel}>Never mind</button>
+              <button data-danger="true" onClick={handleClickDelete}>
+                Delete
+              </button>
+            </footer>
+          </div>
+        </section>
+      )}
+
+      {isDeleteModalOpen && isConfirmation && (
+        <section className="modal">
+          <div className="modal__container">
+            <header>
+              <h2>Are you really sure?</h2>
+            </header>
+            <div className="modal__content">Dude, are you serious?</div>
+            <footer>
+              <button onClick={handleClickCancel}>I regret everything</button>
+              <button data-danger="true" onClick={handleClickConfirmDelete}>
+                Ok then...
+              </button>
+            </footer>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
