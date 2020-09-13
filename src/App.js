a47a7a0cba067d0615fbd191fd4406a8a9b9fb1c
Michell Ayala <starships@outlook.com>
2020-09-12 21:37:30 -0500

    add: delete user modal and delete confirm user modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,88 @@
 import React, { useState } from "react";
 import "./App.css";
 
+function DeleteUserModal({ onClickCancel, onClickAccept }) {
+  return (
+    <Modal
+      header={<ModalHeader title="Delete user" />}
+      content={<div className="modal__content">Dude, are you serious?</div>}
+      footer={
+        <ModalFooter
+          cancelButtonText="Never mind"
+          acceptButtonText="Delete"
+          onClickCancel={onClickCancel}
+          onClickAccept={onClickAccept}
+        />
+      }
+    />
+  );
+}
+
+function ConfirmDeleteUserModal({ onClickClose, onClickAccept }) {
+  return (
+    <Modal
+      header={
+        <ModalHeader
+          title="Are you really sure"
+          hasCloseButton={true}
+          onClickClose={onClickClose}
+        />
+      }
+      footer={
+        <ModalFooter
+          acceptButtonText="Ok then..."
+          onClickAccept={onClickAccept}
+        />
+      }
+    />
+  );
+}
+
+function Modal({ header, content, footer }) {
+  return (
+    <section className="modal">
+      <div className="modal__container">
+        {header}
+        {content}
+        {footer}
+      </div>
+    </section>
+  );
+}
+
+function noop() {}
+
+function ModalHeader({ title, hasCloseButton, onClickClose = noop }) {
+  return (
+    <header className="between">
+      <h2>{title}</h2>
+      {hasCloseButton && (
+        <a onClick={onClickClose} href="#">
+          x
+        </a>
+      )}
+    </header>
+  );
+}
+
+function ModalFooter({
+  cancelButtonText,
+  acceptButtonText,
+  onClickCancel,
+  onClickAccept,
+}) {
+  return (
+    <footer>
+      {cancelButtonText && cancelButtonText.length && (
+        <button onClick={onClickCancel}>{cancelButtonText}</button>
+      )}
+      <button data-danger="true" onClick={onClickAccept}>
+        {acceptButtonText}
+      </button>
+    </footer>
+  );
+}
+
 function App() {
   const [users, setUsers] = useState({
     1: { id: 1, name: "John Doe" },
@@ -43,39 +125,17 @@ function App() {
       ))}
 
       {isDeleteModalOpen && !isConfirmation && (
-        <section className="modal">
-          <div className="modal__container">
-            <header>
-              <h2>Delete User</h2>
-            </header>
-            <div className="modal__content">
-              You are about to remove a user, are you sure about that?
-            </div>
-            <footer>
-              <button onClick={handleClickCancel}>Never mind</button>
-              <button data-danger="true" onClick={handleClickDelete}>
-                Delete
-              </button>
-            </footer>
-          </div>
-        </section>
+        <DeleteUserModal
+          onClickCancel={handleClickCancel}
+          onClickAccept={handleClickDelete}
+        />
       )}
 
       {isDeleteModalOpen && isConfirmation && (
-        <section className="modal">
-          <div className="modal__container">
-            <header>
-              <h2>Are you really sure?</h2>
-            </header>
-            <div className="modal__content">Dude, are you serious?</div>
-            <footer>
-              <button onClick={handleClickCancel}>I regret everything</button>
-              <button data-danger="true" onClick={handleClickConfirmDelete}>
-                Ok then...
-              </button>
-            </footer>
-          </div>
-        </section>
+        <ConfirmDeleteUserModal
+          onClickClose={handleClickCancel}
+          onClickAccept={handleClickConfirmDelete}
+        />
       )}
     </div>
   );
